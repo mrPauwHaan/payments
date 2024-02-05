8f44231b42693eee577563c7fa273b8deb521955
mrPauwHaan <120259765+mrPauwHaan@users.noreply.github.com>
2024-02-05 14:13:09 +0800

    Update mollie_settings.py

--- payments/payment_gateways/doctype/mollie_settings/mollie_settings.py
+++ payments/payment_gateways/doctype/mollie_settings/mollie_settings.py
@@ -148,8 +148,11 @@ class MollieSettings(Document):
 		else:
 			redirect_url = "payment-failed"
 
-		if redirect_to:
+		if redirect_to and "?" in redirect_url:
+			redirect_url += "&" + urlencode({"redirect_to": redirect_to})
+		else:
 			redirect_url += "?" + urlencode({"redirect_to": redirect_to})
+
 		if redirect_message:
 			redirect_url += "&" + urlencode({"redirect_message": redirect_message})
 
