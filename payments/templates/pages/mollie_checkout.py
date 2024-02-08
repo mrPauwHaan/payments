323940d032eda856c152d3707b9c13a2375f8c62
mrPauwHaan <120259765+mrPauwHaan@users.noreply.github.com>
2024-02-08 21:21:59 +0800

    Update mollie_checkout.py

--- payments/templates/pages/mollie_checkout.py
+++ payments/templates/pages/mollie_checkout.py
@@ -65,8 +65,10 @@ def get_header_image(doc, gateway_controller):
 @frappe.whitelist(allow_guest=True)
 def make_payment(data, reference_doctype, reference_docname):
 	data = json.loads(data)
-
+	
 	gateway_controller = get_gateway_controller(reference_doctype, reference_docname)
+	frappe.log_error("voor", data)
 	data = frappe.get_doc("Mollie Settings", gateway_controller).create_request(data)
+	frappe.log_error("na", data)
 	frappe.db.commit()
 	return data
