a1a01e2455d9a2342d99bdb9db81fe3cc6f5e7f3
mrPauwHaan <120259765+mrPauwHaan@users.noreply.github.com>
2024-02-08 22:54:45 +0800

    Update mollie_checkout.js

--- payments/templates/includes/mollie_checkout.js
+++ payments/templates/includes/mollie_checkout.js
@@ -1,39 +1,41 @@
-addEventListener('submit', e => {
-	e.preventDefault();
-	var data = {{ frappe.form_dict | json }};
-	var doctype = "{{ reference_doctype }}"
-	var docname = "{{ reference_docname }}"
-
-	frappe.call({
-		method: "payments.templates.pages.mollie_checkout.make_payment",
-		freeze: true,
-		headers: {
-			"X-Requested-With": "XMLHttpRequest"
-		},
-		args: {
-			"data": JSON.stringify(data),
-			"reference_doctype": doctype,
-			"reference_docname": docname
-		},
-		callback: function(r){
-			if (r.message.status == "Completed") {
-					$('#submit').hide()
-					$('#processing').hide()
-					setTimeout(function() {
-						window.location.href = r.message.redirect_to
-					}, 2000);
-				} else {
+$(document).ready(function() {
+	var form = document.querySelector('#payment-form');
+	form.addEventListener('submit', e => {
+		e.preventDefault();
+		var data = {{ frappe.form_dict | json }};
+		var doctype = "{{ reference_doctype }}"
+		var docname = "{{ reference_docname }}"
+	
+		frappe.call({
+			method: "payments.templates.pages.mollie_checkout.make_payment",
+			freeze: true,
+			headers: {
+				"X-Requested-With": "XMLHttpRequest"
+			},
+			args: {
+				"data": JSON.stringify(data),
+				"reference_doctype": doctype,
+				"reference_docname": docname
+			},
+			callback: function(r){
+				if (r.message.status == "Completed") {
+						$('#submit').hide()
+						$('#processing').hide()
+						setTimeout(function() {
+							window.location.href = r.message.redirect_to
+						}, 2000);
+					} else {
+				}
+				if (r.message.paymentUrl) {
+						$('#submit').hide()
+						$('#processing').show()
+						setTimeout(function() {
+							window.open(r.message.paymentUrl, "_blank");
+						}, 2000);
+					} else {
+				}
 			}
-			if (r.message.paymentUrl) {
-					$('#submit').hide()
-					$('#processing').show()
-					setTimeout(function() {
-						window.open(r.message.paymentUrl, "_blank");
-					}, 2000);
-				} else {
-			}
-		}
-		
+			
+		})
 	})
-
 })
