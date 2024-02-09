ca58824270ee893e5dcc5854ba132ee78a4fd87b
mrPauwHaan <120259765+mrPauwHaan@users.noreply.github.com>
2024-02-09 11:24:36 +0800

    Update mollie_settings.py

--- payments/payment_gateways/doctype/mollie_settings/mollie_settings.py
+++ payments/payment_gateways/doctype/mollie_settings/mollie_settings.py
@@ -121,7 +121,7 @@ class MollieSettings(Document):
             		{
 				'amount': {
         				'currency': self.data.currency,
-        				'value': self.data.amount
+        				'value': cint(flt(self.data.amount) * 100)
     				},
                 		"description": self.data.description,
 				'redirectUrl': self.data.get("redirect_to"),
