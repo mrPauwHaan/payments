6a6b995f65f1790261d1397e143664f7f1c5e57c
mrPauwHaan <120259765+mrPauwHaan@users.noreply.github.com>
2024-02-09 11:05:15 +0800

    Update mollie_settings.py

--- payments/payment_gateways/doctype/mollie_settings/mollie_settings.py
+++ payments/payment_gateways/doctype/mollie_settings/mollie_settings.py
@@ -104,7 +104,7 @@ class MollieSettings(Document):
 			payment = mollie_client.payments.get(paymentID)
 		
 			if payment.is_paid():
-		            	return "Paid"
+		            	return "Completed"
 			elif payment.is_pending():
 				return "Pending"
 			elif payment.is_open():
