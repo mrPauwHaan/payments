21bb595b66ff8e10c497a5b194316c70742f6247
mrPauwHaan <120259765+mrPauwHaan@users.noreply.github.com>
2024-02-09 16:05:01 +0800

    Update mollie_checkout.py

--- payments/templates/pages/mollie_checkout.py
+++ payments/templates/pages/mollie_checkout.py
@@ -66,15 +66,14 @@ def get_header_image(doc, gateway_controller):
 def make_payment(data, reference_doctype, reference_docname):
 	data = json.loads(data)
 	gateway_controller = get_gateway_controller(reference_doctype, reference_docname)
-	data = frappe.get_doc("Mollie Settings", gateway_controller).create_request(data)
-
 	paymentID = frappe.db.get_value(reference_doctype, reference_docname, 'payment_id')
+	
 	if not paymentID:
+		data = frappe.get_doc("Mollie Settings", gateway_controller).create_request(data)
 		paymentID = data.paymentID
 	
 	status = frappe.get_doc("Mollie Settings", gateway_controller).check_request(data, paymentID)
-	frappe.log_error(data)
-	data.status = status
+	data["status"] = status
 	
 	frappe.db.commit()
 	return data
