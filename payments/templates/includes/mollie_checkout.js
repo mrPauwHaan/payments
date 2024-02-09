1a331a17433c257303542ea9d9dacd789cd75e46
mrPauwHaan <120259765+mrPauwHaan@users.noreply.github.com>
2024-02-09 15:15:59 +0800

    Update mollie_checkout.js

--- payments/templates/includes/mollie_checkout.js
+++ payments/templates/includes/mollie_checkout.js
@@ -4,39 +4,6 @@ $(document).ready(function() {
 	var doctype = "{{ reference_doctype }}"
 	var docname = "{{ reference_docname }}"
 	frappe.call({
-		method: "payments.templates.pages.mollie_checkout.make_payment",
-		freeze: true,
-		headers: {
-			"X-Requested-With": "XMLHttpRequest"
-		},
-		args: {
-			"data": JSON.stringify(data),
-			"reference_doctype": doctype,
-			"reference_docname": docname
-		},
-		callback: function(r){
-			payment = r.message
-		}
-		
-	})
-
-	form.addEventListener('submit', e => {
-		e.preventDefault();
-		if (payment.status == "Completed") {
-					$('#submit').hide()
-					setTimeout(function() {
-						window.location.href = r.message.redirect_to
-					}, 2000);
-				} else {
-			}
-		if (payment.paymentUrl) {
-			window.open(payment.paymentUrl, "_blank");
-		}
-	})
-
-	
-	var intervalId = window.setInterval(function(){
-	  	frappe.call({
 			method: "payments.templates.pages.mollie_checkout.check_payment",
 			freeze: true,
 			headers: {
@@ -52,5 +19,18 @@ $(document).ready(function() {
 				document.getElementById("status").value = r.message;
 			}
 		})
-	}, 5000);
+
+	form.addEventListener('submit', e => {
+		e.preventDefault();
+		if (payment.status == "Completed") {
+					$('#submit').hide()
+					setTimeout(function() {
+						window.location.href = r.message.redirect_to
+					}, 2000);
+				} else {
+			}
+		if (payment.paymentUrl) {
+			window.open(payment.paymentUrl, "_blank");
+		}
+	})
 })
